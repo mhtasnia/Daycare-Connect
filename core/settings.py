5ec299985cf8093c5913a470133bfef3ff8c9c2f
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2025-05-21 04:15:17 +0600

    rearranged project

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "auth",
-    "api"
+    "auth_app",
+    "api_app"
 ]
 
 MIDDLEWARE = [
