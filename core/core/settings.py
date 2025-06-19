223e9051bba4b3ec191b533e5962986f42410a5d
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2025-06-19 08:40:56 +0600

    Added testcases for parent side authentication

--- core/core/settings.py
+++ core/core/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 
 
