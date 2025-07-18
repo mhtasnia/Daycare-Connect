bfeb6e2757ecbd772bd92b2f209595710915cdb5
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2025-07-18 19:00:27 +0600

    fixed sonarcloud workflow

--- core/core/settings.py
+++ core/core/settings.py
@@ -95,12 +95,14 @@ CORS_ALLOW_ALL_ORIGINS = True  # For development only!
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get('GITHUB_ACTIONS') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+
 
 
 # Password validation
